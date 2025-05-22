74d0e5c543781127c8d9fb5dac6bd3ec627e9851
chenhong <bobohemian171@gmail.com>
2025-05-22 21:43:58 +0800

    增加图片加载错误重试机制

--- src/components/common/LazyImage.jsx
+++ src/components/common/LazyImage.jsx
@@ -5,21 +5,26 @@ import React, { useState, useEffect, useRef } from 'react';
  * 
  * @param {string} src - 图片URL
  * @param {string} alt - 图片alt文本
- * @param {React.ReactNode} placeholder - 加载占位符组件
  * @param {string} className - 应用于图片的CSS类名
  * @param {Object} imgProps - 传递给img标签的其他属性
  */
 const LazyImage = ({
     src,
     alt = '',
-    placeholder = "placeholder",
     className = '',
     ...imgProps
 }) => {
+
     const [isLoaded, setIsLoaded] = useState(false);
     const [isInView, setIsInView] = useState(false);
     const [hasError, setHasError] = useState(false);
     const imgRef = useRef(null);
+    const retryRef = useRef({
+        count: 0,
+        delay: [500, 1000, 2000],
+        maxCount: 3,
+    });
+
 
     // 使用Intersection Observer API检测图片是否进入视口
     useEffect(() => {
@@ -58,17 +63,27 @@ const LazyImage = ({
     };
 
     // 图片加载失败处理函数
-    const handleImageError = () => {
-        setHasError(true);
-        setIsLoaded(true); // 即使加载失败也标记为已加载，以移除占位符
-    };
-
+    const handleImageError = (img) => {
+        console.log('image load error');
+        console.log(img);
+        const retry = retryRef.current;
+        if (retry.count < retry.maxCount) {
+            setTimeout(() => {
+                retry.count++;
+                img.src = img.src
+                console.log('retry load image');
+            }, retry.delay[retry.count]);
+        } else {
+            setHasError(true);
+            setIsLoaded(true); // 即使加载失败也标记为已加载，以移除占位符}
+        };
+    }
     return (
         <div className={`relative ${className}`} ref={imgRef}>
             {/* 显示占位符，直到图片加载完成 */}
-            {!isLoaded && placeholder && (
+            {!isLoaded  && (
                 <div className="absolute inset-0 flex items-center justify-center ">
-                    {placeholder}
+                    <div className="h-10 w-10 border-2 border-gray-100 border-t-transparent rounded-full animate-spin"></div>
                 </div>
             )}
 
@@ -82,16 +97,19 @@ const LazyImage = ({
             {/* 只有当图片进入视口时才加载图片 */}
             {isInView && !hasError && (
                 <img
-                    src={ src}
+                    src={src}
                     alt={alt}
                     className={`w-full h-full object-cover ${isLoaded ? 'opacity-100' : 'opacity-0'} transition-opacity duration-300`}
                     onLoad={handleImageLoaded}
-                    onError={handleImageError}
+                    onError={(e) => handleImageError(e.target)}
                     {...imgProps}
+
+                    data-retry-count={0}
+                    data-retry-delay={500}
+                    data-max-retries={3}
                 />
             )}
         </div>
-    );
+    )
 };
-
 export default LazyImage;
